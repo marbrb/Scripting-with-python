129ff1b2734719fad1d49ff9f3e04f7ff47dbf43
Miguel Rodriguez <marbrb1@gmail.com>
2016-06-08 19:56:03 -0500

    Zip-File password cracker

---
+++ scriptsEbooks/ZIPcrack.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+
+#Author: Miguel Rodriguez
+#Date: 07/07/2016
+#Description: UNIX password cracker
+#Contact: marbrb1@gmail.com
+
+import zipfile
+from threading import Thread
+import argparse
+
+parser = argparse.ArgumentParser(description="Zip-File password cracker")
+
+parser.add_argument("-f", "--file", dest="zipfile", help="zip-file path", required=True)
+
+arguments = parser.parse_args()
+
+def extractFile(zipFile, password):
+    try:
+        zipFile.extractall(pwd=password)
+        return password
+    except:
+        return
+
+def main():
+    zipFile = zipfile.ZipFile(arguments.zipfile)
+    passFile = open("dictionary.txt", "r")
+    for line in passFile.readlines():
+        password = line.strip("\n")
+        thread = Thread(target=extractFile, args=(zipFile, password))
+        thread.start()
+
+if __name__ == "__main__":
+    main()
