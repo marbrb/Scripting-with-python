99a7b35d00b7b286f47925b5ec2a1933b47088da
Miguel Rodriguez <marbrb1@gmail.com>
2016-12-09 12:17:00 +0000

    removed Adastra scripts

--- scriptsTheHackerWay/CustomWebServer.py
+++
@@ -1,8 +0,0 @@
-from twisted.web.server import Site
-from twisted.web.static import File
-from twisted.internet import reactor
-
-resource = File('/opt/WebServerApacheFull/httpd-2.2.22/htdocs/')
-factory = Site(resource)
-reactor.listenTCP(8080, factory)
-reactor.run()
--- scriptsTheHackerWay/SimpleReverseProxy.py
+++
@@ -1,6 +0,0 @@
-from twisted.internet import reactor
-from twisted.web import proxy, server
-
-site = server.Site(proxy.ReverseProxyResource('www.thehackerway.com', 80, ''))
-reactor.listenTCP(8080, site)
-reactor.run()
